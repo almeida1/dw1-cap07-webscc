9720bab7aeea5927c2fcfe88c2c9ffc779b39872
profe <profe@DESKTOP-EPGIS66>
2023-02-12 14:29:36 -0300

    Refatoracao do controller para manipular json inclusao bib GSON

--- sccweb/src/main/java/com/fatec/sccweb/controller/GUIClienteController.java
+++ sccweb/src/main/java/com/fatec/sccweb/controller/GUIClienteController.java
@@ -38,9 +38,11 @@ public class GUIClienteController {
 
 		try {
 			ResponseEntity<String> resposta = restTemplate.getForEntity(url, String.class);
-			logger.info(">>>>>> consulTodos clientes chamado =>" + resposta.getBody());
+			logger.info(">>>>>> consulTodos clientes chamou api retorno =>" + resposta.getBody());
+			/*
+			 * converte json array para java object 
+			 */
 			Gson gson = new Gson();
-
 			Type tipoLista = new TypeToken<ArrayList<Cliente>>() {	}.getType();
 			ArrayList<Cliente> lista = gson.fromJson(resposta.getBody(), tipoLista);
 			return lista;
--- sccweb/src/test/java/com/fatec/sccweb/REQ01ConsultarCliente.java
+++ sccweb/src/test/java/com/fatec/sccweb/REQ01ConsultarCliente.java
@@ -1,30 +1,48 @@
 package com.fatec.sccweb;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fatec.sccweb.controller.GUIClienteController;
 
+
 @WebMvcTest(GUIClienteController.class)
 class REQ01ConsultarCliente {
 	@Autowired
-	  private MockMvc mvc;
+	private MockMvc mvc;
 
 	@Test
-	void ct01_consultar_todos_clientes_com_sucesso() throws Exception  {
-		mvc.perform(MockMvcRequestBuilders
-				.get("/sig/clientes")
-				.contentType("application/json")
+	void ct01_consultar_todos_clientes_com_sucesso() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/sig/clientes").contentType("application/json")
 				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
-		 
+		.andExpect(status().isOk())
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.view().name("consultarCliente"));
 	}
 
+	@Test
+	void ct02_consultar_todos_clientes_com_sucesso_retorna_pagina_html() throws Exception {
+		// when
+		MockHttpServletResponse response = mvc.perform(get("/sig/clientes").accept(MediaType.APPLICATION_JSON))
+				.andReturn().getResponse();
+
+		// then
+				
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+	}
 }

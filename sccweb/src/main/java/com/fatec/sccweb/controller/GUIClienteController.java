0d3a4adfdbb0930dbbe0d8917f9754a471e8e6b3
profe <profe@DESKTOP-EPGIS66>
2023-02-14 22:07:22 -0300

    atualiza e exclui informacoes de cliente

--- sccweb/src/main/java/com/fatec/sccweb/controller/GUIClienteController.java
+++ sccweb/src/main/java/com/fatec/sccweb/controller/GUIClienteController.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -16,7 +17,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.HttpClientErrorException;
@@ -40,55 +43,97 @@ public class GUIClienteController {
 		mv.addObject("clientes", consultaTodos());
 		return mv;
 	}
+
 	@GetMapping("/cliente")
 	public ModelAndView retornaFormDeCadastroDe(Cliente cliente) {
 		ModelAndView mv = new ModelAndView("cadastrarCliente");
-		
-		
 		mv.addObject("cliente", cliente);
 		return mv;
 	}
+
 	@PostMapping("/clientes")
 	public ModelAndView save(@Valid Cliente cliente, BindingResult result) {
 		ModelAndView mv = new ModelAndView("consultarCliente");
 		if (result.hasErrors()) {
-			
-			
 			mv.setViewName("cadastrarCliente");
 		} else {
 			try {
 				logger.info(">>>>>> controller save chamado");
 				save(cliente);
 				mv.addObject("clientes", consultaTodos());
-			}  catch (ResourceAccessException e) {
+			} catch (ResourceAccessException e) {
 				logger.info(">>>>>> controller save erro Resource Exception => " + e.getMessage());
 				mv.setViewName("cadastrarCliente");
 				mv.addObject("message", "Dados invalidos" + e.getMessage());
 
-			}catch (HttpClientErrorException e) {
+			} catch (HttpClientErrorException e) {
 				logger.info(">>>>>> controller save erro HttpClientErrorException =>" + e.getMessage());
 				mv.setViewName("cadastrarCliente");
 				mv.addObject("message", "Dados invalidos" + e.getMessage());
 
-			} catch(Exception e) {
+			} catch (Exception e) {
 				logger.info(">>>>>> controller cadastrar com dados invalidos =>" + e.getMessage());
 				mv.setViewName("cadastrarCliente");
 				mv.addObject("message", "Dados invalidos" + e.getMessage());
 			}
-			
+
 		}
 		return mv;
+	}
+	@GetMapping("/clientes/{id}") // diz ao metodo que ira responder a uma requisicao do tipo get
+	public ModelAndView retornaFormParaEditarCliente(@PathVariable("id") String id) {
+		ModelAndView mv = new ModelAndView("atualizarCliente");
+	
+		
+		String clienteJson = consultaPorId(id);
+		logger.info(">>>>>> controller form atualiza  => " + clienteJson);
+		Gson gson = new Gson();
+		Optional<Cliente> cliente = Optional.ofNullable(gson.fromJson(clienteJson, Cliente.class)); // deserializes json into target2
+		
+		if (cliente.isPresent()) {
+			mv.addObject("cliente", cliente.get()); // retorna um objeto do tipo cliente
+		} else {
+			return new ModelAndView ("paginaMenu");
+		}
+		return mv; 
+	}
+	@PostMapping("/clientes/id/{id}")
+	public ModelAndView atualiza (Cliente cliente, @PathVariable("id") String id ) {
+		ModelAndView mv = new ModelAndView("consultarCliente");
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8080/api/v1/clientes/" +id;
+		logger.info(">>>>>> atualiza cliente chamado =>" + url);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<Cliente> entity = new HttpEntity<Cliente>(cliente, headers);
+		restTemplate.exchange(url, HttpMethod.PUT, entity, String.class).getBody();
+		mv.addObject("clientes", consultaTodos());
+		return mv;
+	}
+	@GetMapping("/clientes/id2/{id}")
+	public ModelAndView excluirNoFormDeConsultaCliente(@PathVariable("id") Long id) {
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8080/api/v1/clientes/" +id;
+		logger.info(">>>>>> atualiza cliente chamado =>" + url);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<Cliente> entity = new HttpEntity<Cliente>(headers);
+		restTemplate.exchange(url, HttpMethod.DELETE, entity, String.class).getBody();
+		logger.info(">>>>>> 1. servico de exclusao chamado para o id =>  " + id);
+		ModelAndView modelAndView = new ModelAndView("consultarCliente");
+		modelAndView.addObject("clientes", consultaTodos());
+		return modelAndView;
 	}
 	
 	public String save(Cliente cliente) {
 		RestTemplate restTemplate = new RestTemplate();
 		String url = "http://localhost:8080/api/v1/clientes";
 		logger.info(">>>>>> save clientes chamado");
-	    HttpHeaders headers = new HttpHeaders();
-	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	    HttpEntity<Cliente> entity = new HttpEntity<Cliente>(cliente,headers);
-	    return restTemplate.exchange(url, HttpMethod.POST, entity, String.class).getBody();
-		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<Cliente> entity = new HttpEntity<Cliente>(cliente, headers);
+		return restTemplate.exchange(url, HttpMethod.POST, entity, String.class).getBody();
+
 	}
 
 	public List<ClienteDTO> consultaTodos() {
@@ -100,10 +145,11 @@ public class GUIClienteController {
 			ResponseEntity<String> resposta = restTemplate.getForEntity(url, String.class);
 			logger.info(">>>>>> consultaTodos clientes chamou api retorno =>" + resposta.getBody());
 			/*
-			 * converte json array para java object 
+			 * converte json array para java object
 			 */
 			Gson gson = new Gson();
-			Type tipoLista = new TypeToken<ArrayList<ClienteDTO>>() {	}.getType();
+			Type tipoLista = new TypeToken<ArrayList<ClienteDTO>>() {
+			}.getType();
 			ArrayList<ClienteDTO> lista = gson.fromJson(resposta.getBody(), tipoLista);
 			return lista;
 		} catch (ResourceAccessException e) {
@@ -120,5 +166,14 @@ public class GUIClienteController {
 			return null;
 		}
 
+	}
+	public String consultaPorId(String id) {
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8080/api/v1/clientes/" + id ;
+		logger.info(">>>>>> consulta por id chamado => " + url);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity <String> entity = new HttpEntity<String>(headers);
+		return restTemplate.exchange(url, HttpMethod.GET, entity, String.class).getBody();
 	}
 }
